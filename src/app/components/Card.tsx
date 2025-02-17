dedf646d0db253db0c8eb83552328f8830d6eef4
Guilherme-Dias-gomes <guilhermediasgomes2@gmail.com>
2025-02-17 17:35:48 -0300

    adicionando page "Sobre"

--- src/app/components/Card.tsx
+++ src/app/components/Card.tsx
@@ -5,7 +5,7 @@ interface CardProps {
 
 export default function Card({title, content}: CardProps) {
     return(
-        <div className="border-2 border-black py-56 px-20 bg-nude rounded-xl text-center">
+        <div className="py-56 px-20 bg-nude rounded-xl text-center">
             <h1 className="text-xl font-bold">{title}</h1>
             <p className="text-gray-600 mt-2">{content}</p>
         </div>
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -19,10 +19,8 @@ export default function Home() {
     <div className="p-0 bg-[url('/background.svg')] bg-cover bg-center font-cinzel">
       <header
         id="navbar"
-        className={`sticky top-0 flex flex-row justify-around items-center p-5 z-10 font-sans transition-all duration-300 ${
-          scrolled
-            ? "bg-marrom shadow-md"
-            : "bg-transparent text-black"
+        className={`sticky top-0 flex flex-row justify-around items-center p-2 z-10 font-sans transition-all duration-300 ${
+          scrolled ? "bg-marrom shadow-md" : "bg-transparent text-black"
         }`}
       >
         <div className="w-48 h-1/2">
@@ -40,13 +38,13 @@ export default function Home() {
       </header>
       <section className="flex h-screen justify-evenly items-center">
         <div className="flex w-1/2 flex-col gap-4 ">
-          <h1 className="text-4xl pl-14">
+          <h1 className="text-6xl pl-20">
             Realce sua beleza natural e aumente sua autoestima com um cuidado
             completo da pele
           </h1>
           <a
             href=""
-            className="border ml-14 border-black p-4 w-1/3 font-bold rounded-full text-center bg-nude"
+            className="ml-14 p-4 w-1/3 font-bold rounded-full text-center bg-nude"
           >
             AGENDAR CONSULTA
           </a>
@@ -58,19 +56,77 @@ export default function Home() {
       <section className="flex flex-col h-screen bg-marrom items-center justify-around">
         <h1 className="font-bold text-4xl">Procedimentos</h1>
         <div className="flex justify-around w-full">
-          <Card title="BOTOX" content="Este é meu teste de components"/>
-          <Card title="Preenchimento labial" content="Este é meu teste de components"/>
-          <Card title="Limpeza de pele" content="Este é meu teste de components"/>
-          <Card title="Vasos" content="Este é meu teste de components"/>
+          <Card title="BOTOX" content="Este é meu teste de components" />
+          <Card
+            title="Preenchimento labial"
+            content="Este é meu teste de components"
+          />
+          <Card
+            title="Limpeza de pele"
+            content="Este é meu teste de components"
+          />
+          <Card title="Vasos" content="Este é meu teste de components" />
         </div>
         <a
-            href=""
-            className="border ml-14 border-black p-4 w-1/3 font-bold rounded-full text-center bg-nude"
-          >
-            AGENDAR CONSULTA
-          </a>
+          href=""
+          className="ml-14 p-4 w-1/3 font-bold rounded-full text-center bg-nude"
+        >
+          AGENDAR CONSULTA
+        </a>
       </section>
-      <section className="flex h-screen"></section>
+      <section className="flex h-screen items-center justify-around">
+        <div className="flex items-center w-4/5 text-center">
+          <div className="py-80 px-60 bg-gray-400 rounded-3xl"></div>
+          <div className="flex flex-col h-1/2 p-6  gap-4 w-1/2">
+            <h1 className="font-bold text-4xl">
+              Venha conhecer a Dra. Rosiane Gomes
+            </h1>
+            <p className=" text-2xl">
+              Um espaço cuidadosamente projetado para proporcionar uma
+              experiência aconchegante, focada no autocuidado e no bem-estar{" "}
+            </p>
+            <div className="flex justify-center">
+              <a
+                href=""
+                className="p-4 font-bold rounded-full text-center bg-nude"
+              >
+                DESCUBRA SUA MELHOR VERSÃO
+              </a>
+            </div>
+          </div>
+        </div>
+        <div className="h-1/3">
+          <div className="w-56 ">
+            <img src="teste5.0.png" alt="logo" />
+          </div>
+        </div>
+      </section>
+      <section className="flex h-screen bg-marrom items-center justify-around">
+        <div className="flex flex-col gap-10 w-1/3">
+          <h1 className="text-5xl">Dra. Rosiane Gomes</h1>
+          <div className="flex items-center gap-4 text-xl border-b border-black p-2">
+            <img src="correto.png" alt="correto" className="w-10" />
+            <p>Enfermeira graduada pela Universidade Italo, em São Paulo - SP.</p>
+          </div>
+          <div className="flex items-center gap-4 text-xl border-b border-black p-2">
+            <img src="correto.png" alt="correto" className="w-10" />
+            <p>Possui especialização em Dermatologia pela Instituição Superior de Medicina Dermatológica (ISMD).</p>
+          </div>
+          <div className="flex items-center gap-4 text-xl border-b border-black p-2">
+            <img src="correto.png" alt="correto" className="w-10" />
+            <p>Especialista em Dermatologia Estética, Cirúrgica e Clínica</p>
+          </div>
+          <div className="flex justify-center">
+              <a
+                href=""
+                className="p-4 font-bold rounded-full text-center bg-nude"
+              >
+                DESCUBRA SUA MELHOR VERSÃO
+              </a>
+            </div>
+        </div>
+        <div className="py-80 px-60 bg-gray-400 rounded-3xl"></div>
+      </section>
     </div>
   );
 }
